e28a3fff71453f5cf913a334af71eb131dd3445b
Suraj sahani <116185940+SurajSahani0011@users.noreply.github.com>
2024-02-29 18:47:09 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import connectToMongo from './config/db.js';
+import cors from 'cors'
+import routes from './routes/TodoRoute.js'
+import path from 'path';
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname,"..","client/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","client","build","index.html"))
+})
+app.use(cors());
+connectToMongo()
+const PORT =8000;
+
+app.use(routes)
+app.listen(PORT,()=>{
+    console.log(`server listening on ${PORT}`)
+})
