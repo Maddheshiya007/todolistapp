400a4958c23c110eb3c793e92414b67b04f75ec6
Suraj sahani <116185940+SurajSahani0011@users.noreply.github.com>
2024-03-01 00:28:32 +0530

    s

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"..","client","build")));
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"..","build","index.html"))
+    res.sendFile(path.join(__dirname,"..","client","build","index.html"))
 })
 app.use(cors());
 connectToMongo()
